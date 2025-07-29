8bd5b6ccff8755bcf94e5c7b96d0fb2a6603f024
Marcelo de la Barra <marcelodelabarra@MacBook-Pro-de-Marcelo.local>
2025-07-29 13:36:48 -0400

    feat: add enums and modify index of enums

---
+++ lib/models/enums/bankAccounts.ts
@@ -0,0 +1,36 @@
+export enum ArgentinaBankAccountKind {
+  CORRIENTE = "corriente",
+  VISTA = "vista",
+  AHORRO = "ahorro",
+  ELECTRONIC_CHECKBOOK = "electronic_checkbook",
+  AR_CBU = "ar_cbu",
+  AR_CVU = "ar_cvu",
+  AR_ALIAS = "ar_alias",
+}
+
+export enum BrazilBankAccountKind {
+  BR_CORRIENTE_FISICA = "br_corriente_fisica",
+  BR_SIMPLE_FISICA = "br_simple_fisica",
+  BR_CORRIENTE_JURIDICA = "br_corriente_juridica",
+  BR_POUPANCA_FISICA = "br_poupanca_fisica",
+  BR_POUPANCA_JURIDICA = "br_poupanca_juridica",
+  BR_CAIXA_FACIL = "br_caixa_facil",
+  BR_PIX = "br_pix",
+}
+
+export enum ChileBankAccountKind {
+  CORRIENTE = "corriente",
+  VISTA = "vista",
+  AHORRO = "ahorro",
+  ELECTRONIC_CHECKBOOK = "electronic_checkbook",
+}
+
+export enum PeruBankAccountKind {
+  CORRIENTE = "corriente",
+  AHORRO = "ahorro",
+}
+
+export enum ColombiaBankAccountKind {
+  CORRIENTE = "corriente",
+  AHORRO = "ahorro",
+}
